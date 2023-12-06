f06996b11f1bebde28e66abade9f44e3c1245429
fabio0604 <129704222+fabio0604@users.noreply.github.com>
2023-12-06 20:47:51 -0300

    a

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
+// Função para adicionar produto ao carrinho
+function adicionarAoCarrinho(produto, preco) {
+  // Esta é uma simulação simples, na prática você enviaria esses dados para um carrinho de compras ou serviço back-end
+  console.log(`"${produto}" foi adicionado ao carrinho por R$${preco}.`);
+}
+
 // Obtemos todos os botões de compra
 const buyButtons = document.querySelectorAll('.buy-button');
 
 // Adicionamos um evento de clique a cada botão
 buyButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    // Aqui você pode adicionar a lógica desejada para a ação de compra
-    // Por exemplo, adicionar o produto ao carrinho ou redirecionar para o checkout
-    // Neste exemplo, apenas exibiremos uma mensagem no console
-    console.log('Produto adicionado ao carrinho!');
+  button.addEventListener('click', (event) => {
+    // Obtemos os dados do produto a partir do atributo 'data' do botão
+    const produto = button.getAttribute('data-product');
+    const preco = parseFloat(button.getAttribute('data-price'));
+
+    // Chamamos a função para adicionar ao carrinho com os dados do produto
+    adicionarAoCarrinho(produto, preco);
   });
 });
