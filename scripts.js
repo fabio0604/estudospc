6856f280cc0e5ba85dedc3917a7d60a48b72cf23
fabio0604 <129704222+fabio0604@users.noreply.github.com>
2023-12-06 21:48:03 -0300

    a

--- scripts.js
+++ scripts.js
@@ -83,3 +83,35 @@ checkoutButton.addEventListener('click', () => {
     alert('Seu carrinho está vazio. Adicione itens antes de finalizar a compra.');
   }
 });
+
+// Função para adicionar produto ao carrinho
+function adicionarAoCarrinho(produto, preco) {
+  const carrinho = document.querySelector('.cart-items');
+
+  // Criar um novo item para o carrinho
+  const novoItem = document.createElement('li');
+  novoItem.innerText = `${produto} - R$${preco.toFixed(2)}`;
+
+  // Adicionar o novo item à lista de itens do carrinho
+  carrinho.appendChild(novoItem);
+
+  // Atualizar o total do carrinho
+  const totalAtual = parseFloat(document.querySelector('.cart-total').innerText);
+  const novoTotal = totalAtual + preco;
+  document.querySelector('.cart-total').innerText = novoTotal.toFixed(2);
+}
+
+// Obtemos todos os botões de compra
+const buyButtons = document.querySelectorAll('.buy-button');
+
+// Adicionamos um evento de clique a cada botão
+buyButtons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    // Obtemos os dados do produto a partir do atributo 'data' do botão
+    const produto = button.getAttribute('data-product');
+    const preco = parseFloat(button.getAttribute('data-price'));
+
+    // Chamamos a função para adicionar ao carrinho com os dados do produto
+    adicionarAoCarrinho(produto, preco);
+  });
+});
